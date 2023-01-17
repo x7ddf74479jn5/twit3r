9dbf73e0f9910a5015ebb4b2c71b4816a8e7b221
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-01-18 02:04:03 +0900

    ➕ add: tanstack devtools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { api } from "../utils/api";
 
@@ -20,6 +21,8 @@ const MyApp: AppType<{ session: Session | null }> = ({
         </main>
       </Container>
       <LoggedOutBanner />
+
+      <ReactQueryDevtools initialIsOpen={false} />
     </SessionProvider>
   );
 };
