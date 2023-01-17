71f7a5704cf2c21205ea5b7c1330e356ca9340f1
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-01-17 18:27:32 +0900

    ✨ feat: tweet create router

---
+++ src/server/api/routers/tweet.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const tweetRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(
+      z.object({
+        text: z.string({
+          required_error: "Tweet text is required",
+        }),
+      })
+    )
+    .mutation(({ ctx, input }) => {
+      const { prisma, session } = ctx;
+      const { text } = input;
+
+      const userId = session.user.id;
+
+      return prisma.tweet.create({
+        data: {
+          text,
+          author: {
+            connect: {
+              id: userId,
+            },
+          },
+        },
+      });
+    }),
+});
