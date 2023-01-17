6e60e6f08845286da5f244adc54a42d3ef885ae1
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-01-18 01:15:56 +0900

    ♻️ refactor: tweet router

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 import { tweetSchema } from "../../../components/CreateTweet";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const tweetRouter = createTRPCRouter({
   create: protectedProcedure.input(tweetSchema).mutation(({ ctx, input }) => {
@@ -21,4 +21,47 @@ export const tweetRouter = createTRPCRouter({
       },
     });
   }),
+
+  timeline: publicProcedure
+    .input(
+      z.object({
+        cursor: z.string().nullish(),
+        limit: z.number().min(1).max(100).default(10),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { prisma } = ctx;
+      const { cursor, limit } = input;
+
+      const tweets = await prisma.tweet.findMany({
+        take: limit + 1,
+        orderBy: [
+          {
+            createdAt: "desc",
+          },
+        ],
+        cursor: cursor ? { id: cursor } : undefined,
+        include: {
+          author: {
+            select: {
+              name: true,
+              id: true,
+              image: true,
+            },
+          },
+        },
+      });
+
+      let nextCursor: typeof cursor | undefined = undefined;
+
+      if (tweets.length > limit) {
+        const nextItem = tweets.pop() as (typeof tweets)[number];
+        nextCursor = nextItem.id;
+      }
+
+      return {
+        tweets,
+        nextCursor,
+      };
+    }),
 });
