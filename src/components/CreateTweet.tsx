85cca822e6d0a29be3368c94e75983082e7ad2d9
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-01-17 19:50:50 +0900

    🚧 wip: tweet component

---
+++ src/components/CreateTweet.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { z } from "zod";
+
+import { api } from "../utils/api";
+
+export const tweetSchema = z.object({
+  text: z
+    .string({
+      required_error: "Tweet text is required",
+    })
+    .min(10)
+    .max(128),
+});
+export const CreateTweet = () => {
+  const [text, setText] = useState("");
+  const [error, setError] = useState("");
+
+  const { mutateAsync } = api.tweet.create.useMutation();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      await tweetSchema.parse({ text });
+    } catch (e) {
+      if (e instanceof Error) {
+        setError(e.message);
+      }
+      return;
+    }
+
+    if (text.length < 10) {
+      setError("Tweet must be at least 10 characters long");
+      return;
+    }
+
+    mutateAsync({ text });
+  };
+
+  return (
+    <>
+      {error && JSON.stringify(error)}
+      <form
+        onSubmit={handleSubmit}
+        className="2 flex w-full flex-col rounded-md border-2 p-4"
+      >
+        <textarea
+          className="w-full p-4 shadow"
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+        <div className="mt-4 flex justify-end">
+          <button
+            className="bg-primary rounded-md px-4 py-2 text-white"
+            type="submit"
+          >
+            Tweet
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -1,31 +1,24 @@
 import { z } from "zod";
 
+import { tweetSchema } from "../../../components/CreateTweet";
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const tweetRouter = createTRPCRouter({
-  create: protectedProcedure
-    .input(
-      z.object({
-        text: z.string({
-          required_error: "Tweet text is required",
-        }),
-      })
-    )
-    .mutation(({ ctx, input }) => {
-      const { prisma, session } = ctx;
-      const { text } = input;
+  create: protectedProcedure.input(tweetSchema).mutation(({ ctx, input }) => {
+    const { prisma, session } = ctx;
+    const { text } = input;
 
-      const userId = session.user.id;
+    const userId = session.user.id;
 
-      return prisma.tweet.create({
-        data: {
-          text,
-          author: {
-            connect: {
-              id: userId,
-            },
+    return prisma.tweet.create({
+      data: {
+        text,
+        author: {
+          connect: {
+            id: userId,
           },
         },
-      });
-    }),
+      },
+    });
+  }),
 });
